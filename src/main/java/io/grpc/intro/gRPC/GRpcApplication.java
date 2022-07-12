2600341eed6eeabde52d471ea9d201092d125164
Hardik Gunjal <hardikgunjal@Hardiks-MacBook-Pro.local>
2022-07-12 14:18:52 -0400

    generated resources with grpc maven plugin

--- src/main/java/io/grpc/intro/gRPC/GRpcApplication.java
+++ src/main/java/io/grpc/intro/gRPC/GRpcApplication.java
@@ -3,6 +3,17 @@ package io.grpc.intro.gRPC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Defining the Service
+ * We start by defining a service, specifying methods
+ * that can be called remotely along with their parameters
+ * and return types
+ * This is done in the .proto file using the protocol
+ * buffers. They are also used for describing the
+ * structure of the payload messages.
+ *
+ *
+ */
 @SpringBootApplication
 public class GRpcApplication {
 
